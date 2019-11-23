bc63769329f75c268fade24736417b61a3dcbc06
Angela Weber <angelaweber95@hotmail.com>
2019-11-23 12:18:55 -0800

    add async to main js

--- src/main.js
+++ src/main.js
@@ -6,11 +6,28 @@ import './styles.css';
 import { DoctorSearch } from './doctor.js';
 
 $(document).ready(function(){
+  let doctorSearch = new DoctorSearch
   $('form#doctorForm').submit(function(event){
     event.preventDefault();
     let doctorNameInput = $('input#doctorName').val();
     let medicalIssueInput = $('input#medicalIssue').val();
-    
+    (async () => {
+      const response = await doctorSearch.getDoctorsByname(doctorNameInput);
+      getElement(response);
+    })();
+    (async () => {
+      const response2 = await doctorSearch.getDoctorByMedicalIssue(medicalIssueInput);
+      getElement2(response2)
+    })();
+
+    function getElement(response){
+      $('.doctorNameOutput').append()
+    }
+    function getElement2(response2) {
+      $('.medicalIssueOutput').append()
+    }
+
+
   });
   $('input#doctorName').click(function(){
     $('.doctorNameOption').show();
