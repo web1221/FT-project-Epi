2d6b2741ff44b8ef5df283b809554a52ea7c7cb5
Angela Weber <angelaweber95@hotmail.com>
2019-11-23 13:59:08 -0800

    adchange around main.js, and add a loop to loop through name of doctors

--- src/main.js
+++ src/main.js
@@ -1,21 +1,20 @@
-
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { DoctorSearch } from './doctor.js';
+import {
+  DoctorSearch
+} from './doctor.js';
 
-$(document).ready(function(){
-  let doctorSearch = new DoctorSearch
-  $('form#doctorForm').submit(function(event){
+$(document).ready(function() {
+  let doctorSearch = new DoctorSearch;
+  $('form#doctorForm').submit(function(event) {
     event.preventDefault();
     let doctorNameInput = $('input#doctorNameInput').val();
     let medicalIssueInput = $('input#medicalIssueInput').val();
-    console.log(medicalIssueInput);
-    console.log(doctorNameInput);
+
     (async () => {
       const response = await doctorSearch.getDoctorsByname(doctorNameInput);
-      // console.log(response);
       getElement(response);
     })();
     (async () => {
@@ -23,21 +22,41 @@ $(document).ready(function(){
       getElement2(response2);
     })();
 
-    function getElement(response){
-      $('.doctorNameOutput').append();
+    function getElement(response) {
+      console.log(response);
+      for (let i = 0; i < 10; i++) {
+        $('.doctorByNameOutput').append(`<li>${response.data[i].name}</li>`);
+      }
     }
+
     function getElement2(response2) {
       console.log(response2);
-      $('.medicalIssueOutput').append();
+      for (let i = 0; i < 10; i++) {
+        $('.medicalIssueOutput').append(`<li>${response2.data[i].profile.first_name} ${response2.data[i].profile.last_name}</li>`);
+      }
     }
+    $('input#doctorName').click(function() {
+      $('.doctorNameOption').show();
+      $('.medicalIssueOption').hide();
+      (async () => {
+        const response2 = await doctorSearch.getDoctorByMedicalIssue(medicalIssueInput);
+        getElement2(response2);
+      })();
 
+      function getElement2(response2) {
+        console.log(response2);
+        for (let i = 0; i < 10; i++) {
+          $('.medicalIssueOutput').append(`<li>${response2.data[i].profile.first_name} ${response2.data[i].profile.last_name}</li>`);
+        }
+      }
+    });
 
   });
-  $('input#doctorName').click(function(){
+  $('input#doctorName').click(function() {
     $('.doctorNameOption').show();
     $('.medicalIssueOption').hide();
   });
-  $('input#medicalIssue').click(function(){
+  $('input#medicalIssue').click(function() {
     $('.medicalIssueOption').show();
     $('.doctorNameOption').hide();
   });
