41acb5c8af93ac462ce694b11d656219ff203a48
Angela Weber <angelaweber95@hotmail.com>
2019-11-24 12:17:11 -0800

    try and add an error to throw when a name given has no vaule:

--- src/main.js
+++ src/main.js
@@ -7,27 +7,22 @@ import {DoctorIssueSearch} from './doctorByIssue.js';
 
 
 function getElement(response) {
-  console.log(response);
-  for (let i = 0; i < 10; i++) {
-    $('ul#list').append(`<li id=${i}>${response.data[i].name}</li>`);
+  if(response.data[0].name === undefined){
+    return "No doctor";
+  } else{
+    for (let i = 0; i < 10; i++) {
+      $('ul#list').append(`<li id=${i}>${response.data[i].name}</li>`);
+    }
   }
 }
+
 function getElement2(response2) {
   console.log(response2);
-  if
   for (let i = 0; i < 10; i++) {
-    let j = 0;
-    if(response2.data[i].practices[0].within_search_area === false){
-      j++;
-      return $('ul#list').append(`<li id=${i}>${response2.data[i].profile.first_name} ${response2.data[i].profile.last_name}<p>Address: ${response2.data[i].practices[1].visit_address.street} ${response2.data[i].practices[1].visit_address.street2},  ${response2.data[i].practices[1].visit_address.city}, ${response2.data[i].practices[1].visit_address.state_long} ${response2.data[i].practices[1].visit_address.zip}<p></li>`)
-    } else{
-      $('ul#list').append(`<li id=${i}>${response2.data[i].profile.first_name} ${response2.data[i].profile.last_name}<p>Address: ${response2.data[i].practices[j].visit_address.street} ${response2.data[i].practices[j].visit_address.street2},  ${response2.data[i].practices[j].visit_address.city}, ${response2.data[i].practices[j].visit_address.state_long} ${response2.data[i].practices[j].visit_address.zip}<p></li>`)
-    }
-
-    // $('ul#list').append(`<li id=${i}>${response2.data[i].profile.first_name} ${response2.data[i].profile.last_name}<p>Address: ${response2.data[i].practices[0].visit_address.street} ${response2.data[i].practices[0].visit_address.street2},  ${response2.data[i].practices[0].visit_address.city}, ${response2.data[i].practices[0].visit_address.state_long} ${response2.data[i].practices[0].visit_address.zip}<p></li>`)
-
+    $('ul#list').append(`<li id=${i}>${response2.data[i].profile.first_name} ${response2.data[i].profile.last_name}<p>Address: ${response2.data[i].practices[0].visit_address.street} ${response2.data[i].practices[0].visit_address.street2},  ${response2.data[i].practices[0].visit_address.city}, ${response2.data[i].practices[0].visit_address.state_long} ${response2.data[i].practices[0].visit_address.zip}<p></li>`)
   }
 }
+
 
 
 $(document).ready(function() {
@@ -44,7 +39,11 @@ $(document).ready(function() {
       (async () => {
         let doctorNameSearch = new DoctorNameSearch();
         const response = await doctorNameSearch.getDoctorsByname(doctorNameInput);
-        getElement(response);
+        if(doctorNameInput === undefined){
+          throw new Error('no doctors')
+        }else{
+          getElement(response);
+        }
       })();
     } else if (doctorNameInput === "") {
       (async () => {
