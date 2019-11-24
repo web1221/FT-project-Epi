334da921a76cba6d7d490e03ad1c801c34ced2d0
Angela Weber <angelaweber95@hotmail.com>
2019-11-24 13:01:46 -0800

    add more things to append for name lookup

--- src/doctorByName.js
+++ src/doctorByName.js
@@ -1,7 +1,7 @@
 export class DoctorNameSearch{
   async getDoctorsByname(name){
     try {
-      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/practices?name=${name}&location=45.512794,-122.679565,10&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/practices?name=${name}&location=45.512794,-122.679565,20&user_location=45.512794,-122.679565&skip=0&limit=10&user_key=${process.env.API_KEY}`);
       let jsonifiedResponse = await response.json();
       return jsonifiedResponse;
     } catch(error) {
--- src/main.js
+++ src/main.js
@@ -7,8 +7,12 @@ import {DoctorIssueSearch} from './doctorByIssue.js';
 
 
 function getElement(response) {
+  console.log(response);
   for (let i = 0; i < 10; i++) {
-    $('ul#list').append(`<li id=${i}>${response.data[i].name}</li>`);
+    if(response.data[i].within_search_area === false){
+      continue;
+    }
+    $('ul#list').append(`<li> ${response.data[i].name}<p>Address: ${response.data[i].visit_address.street} ${response.data[i].visit_address.city} ${response.data[i].visit_address.state_long} ${response.data[i].visit_address.zip} <br>Phone Number: ${response.data[i].phones[0].number} <br> Accepting New Patients: ${response.data[i].accepts_new_patients}</p></li>`);
   }
 }
 
@@ -20,7 +24,7 @@ function getElement2(response2) {
     if(response2.data[i].practices[0].within_search_area === false){
       continue;
     }
-     $('ul#list').append(`<li id=${i}>${response2.data[i].profile.first_name} ${response2.data[i].profile.last_name}<p>Address: ${response2.data[i].practices[0].visit_address.street}, ${response2.data[i].practices[0].visit_address.city}, ${response2.data[i].practices[0].visit_address.state_long} ${response2.data[i].practices[0].visit_address.zip}<p></li>`)
+     $('ul#list').append(`<li ${response2.data[i].profile.first_name} ${response2.data[i].profile.last_name}<p>Address: ${response2.data[i].practices[0].visit_address.street}, ${response2.data[i].practices[0].visit_address.city}, ${response2.data[i].practices[0].visit_address.state_long} ${response2.data[i].practices[0].visit_address.zip} <br>Phone Number: ${response2.data[i].practices[0].phones[0].number} <br>Accepting New Patients: ${response2.data[i].practices[0].accepts_new_patients} </p></li>`)
   }
 }
 
