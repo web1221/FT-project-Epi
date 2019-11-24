9407db6bfa2f1a5a0b4514e9eac5ee94ea46bf09
Angela Weber <angelaweber95@hotmail.com>
2019-11-23 16:09:03 -0800

    add address to append

--- src/main.js
+++ src/main.js
@@ -2,28 +2,29 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import {
-  DoctorNameSearch
-} from './doctorByName.js';
-import {
-  DoctorIssueSearch
-} from './doctorByIssue.js';
+import {DoctorNameSearch} from './doctorByName.js';
+import {DoctorIssueSearch} from './doctorByIssue.js';
+
+// ${response2.data[i].practices[0].visit_address.city}
 function getElement(response) {
   console.log(response);
   for (let i = 0; i < 10; i++) {
-    $('.doctorByNameOutput').append(`<li>${response.data[i].name}</li>`);
+    $('ul#list').append(`<li id=${i}>${response.data[i].name}</li>`);
   }
 }
 function getElement2(response2) {
   console.log(response2);
   for (let i = 0; i < 10; i++) {
-    $('.medicalIssueOutput').append(`<li>${response2.data[i].profile.first_name} ${response2.data[i].profile.last_name}</li>`);
+    $('ul#list').append(`<li id=${i}>${response2.data[i].profile.first_name} ${response2.data[i].profile.last_name}<p>Address: ${response2.data[i].practices[0].visit_address.street} ${response2.data[i].practices[0].visit_address.street2},  ${response2.data[i].practices[0].visit_address.city}, ${response2.data[i].practices[0].visit_address.state_long} ${response2.data[i].practices[0].visit_address.zip}<p></li>`)
   }
 }
 
+
 $(document).ready(function() {
   $('form#doctorForm').submit(function(event) {
+    // attachListeners();
     event.preventDefault();
+    $('ul').html("");
     let doctorNameInput = $('input#doctorNameInput').val();
     let medicalIssueInput = $('input#medicalIssueInput').val();
     $('input#doctorNameInput, input#medicalIssueInput').val("")
@@ -42,7 +43,6 @@ $(document).ready(function() {
         getElement2(response2);
       })();
     }
-
   });
   $('input#doctorName').click(function() {
     $('.doctorNameOption').show();
