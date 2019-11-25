dbb10373228723a09d0fc1750d0227e0fa71af7c
Angela Weber <angelaweber95@hotmail.com>
2019-11-24 17:27:13 -0800

    add a catch to type error

--- src/doctorByName.js
+++ src/doctorByName.js
@@ -2,13 +2,10 @@ export class DoctorNameSearch{
   async getDoctorsByname(name){
     try {
       let response = await fetch(`https://api.betterdoctor.com/2016-03-01/practices?name=${name}&location=45.512794,-122.679565,20&user_location=45.512794,-122.679565&skip=0&limit=10&user_key=${process.env.API_KEY}`);
-      let jsonifiedResponse = await response.json();
-      return jsonifiedResponse;
-    } catch(error) {
-      console.error("There was an error:" + error.message);
-    }
-    if(name === undefined){
-      return "No Doctor"
+        let jsonifiedResponse = await response.json();
+        return jsonifiedResponse;
+      } catch(error) {
+        console.error("There was an error:" + error.message);
+      }
     }
   }
-}
--- src/main.js
+++ src/main.js
@@ -8,11 +8,17 @@ import {DoctorIssueSearch} from './doctorByIssue.js';
 
 function getElement(response) {
   console.log(response);
-  for (let i = 0; i < 10; i++) {
-    if(response.data[i].within_search_area === false){
-      continue;
+  try{
+    for (let i = 0; i < 10; i++) {
+      if(response.data[i].within_search_area === false){
+        continue;
+      }
+      $('ul#list').append(`<li> ${response.data[i].name}<p>Address: ${response.data[i].visit_address.street} ${response.data[i].visit_address.city} ${response.data[i].visit_address.state_long} ${response.data[i].visit_address.zip} <br>Phone Number: ${response.data[i].phones[0].number} <br> Accepting New Patients: ${response.data[i].accepts_new_patients}</p></li>`);
     }
-    $('ul#list').append(`<li> ${response.data[i].name}<p>Address: ${response.data[i].visit_address.street} ${response.data[i].visit_address.city} ${response.data[i].visit_address.state_long} ${response.data[i].visit_address.zip} <br>Phone Number: ${response.data[i].phones[0].number} <br> Accepting New Patients: ${response.data[i].accepts_new_patients}</p></li>`);
+  } catch(response){
+    if(response instanceof TypeError){
+      return $('ul#list').append(`<p>Sorry </p>`)
+    }
   }
 }
 
@@ -24,7 +30,7 @@ function getElement2(response2) {
     if(response2.data[i].practices[0].within_search_area === false){
       continue;
     }
-     $('ul#list').append(`<li ${response2.data[i].profile.first_name} ${response2.data[i].profile.last_name}<p>Address: ${response2.data[i].practices[0].visit_address.street}, ${response2.data[i].practices[0].visit_address.city}, ${response2.data[i].practices[0].visit_address.state_long} ${response2.data[i].practices[0].visit_address.zip} <br>Phone Number: ${response2.data[i].practices[0].phones[0].number} <br>Accepting New Patients: ${response2.data[i].practices[0].accepts_new_patients} </p></li>`)
+    $('ul#list').append(`<li ${response2.data[i].profile.first_name} ${response2.data[i].profile.last_name}<p>Address: ${response2.data[i].practices[0].visit_address.street}, ${response2.data[i].practices[0].visit_address.city}, ${response2.data[i].practices[0].visit_address.state_long} ${response2.data[i].practices[0].visit_address.zip} <br>Phone Number: ${response2.data[i].practices[0].phones[0].number} <br>Accepting New Patients: ${response2.data[i].practices[0].accepts_new_patients} </p></li>`)
   }
 }
 
@@ -44,11 +50,7 @@ $(document).ready(function() {
       (async () => {
         let doctorNameSearch = new DoctorNameSearch();
         const response = await doctorNameSearch.getDoctorsByname(doctorNameInput);
-        if(doctorNameInput === undefined){
-          throw new Error('no doctors')
-        }else{
-          getElement(response);
-        }
+        getElement(response);
       })();
     } else if (doctorNameInput === "") {
       (async () => {
