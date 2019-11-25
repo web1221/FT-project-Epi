38103087155a496eb7cec4b288448cab78b54534
Angela Weber <angelaweber95@hotmail.com>
2019-11-24 17:44:16 -0800

    add try catch to getElement2 function:

--- src/doctorByName.js
+++ src/doctorByName.js
@@ -1,11 +1,11 @@
-export class DoctorNameSearch{
-  async getDoctorsByname(name){
+export class DoctorNameSearch {
+  async getDoctorsByname(name) {
     try {
       let response = await fetch(`https://api.betterdoctor.com/2016-03-01/practices?name=${name}&location=45.512794,-122.679565,20&user_location=45.512794,-122.679565&skip=0&limit=10&user_key=${process.env.API_KEY}`);
-        let jsonifiedResponse = await response.json();
-        return jsonifiedResponse;
-      } catch(error) {
-        console.error("There was an error:" + error.message);
-      }
+      let jsonifiedResponse = await response.json();
+      return jsonifiedResponse;
+    } catch (error) {
+      console.error("There was an error:" + error.message);
     }
   }
+}
--- src/main.js
+++ src/main.js
@@ -2,22 +2,21 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import {DoctorNameSearch} from './doctorByName.js';
-import {DoctorIssueSearch} from './doctorByIssue.js';
+import { DoctorNameSearch } from './doctorByName.js';
+import { DoctorIssueSearch } from './doctorByIssue.js';
 
 
 function getElement(response) {
-  console.log(response);
-  try{
+  try {
     for (let i = 0; i < 10; i++) {
-      if(response.data[i].within_search_area === false){
+      if (response.data[i].within_search_area === false) {
         continue;
       }
       $('ul#list').append(`<li> ${response.data[i].name}<p>Address: ${response.data[i].visit_address.street} ${response.data[i].visit_address.city} ${response.data[i].visit_address.state_long} ${response.data[i].visit_address.zip} <br>Phone Number: ${response.data[i].phones[0].number} <br> Accepting New Patients: ${response.data[i].accepts_new_patients}</p></li>`);
     }
-  } catch(response){
-    if(response instanceof TypeError){
-      return $('ul#list').append(`<p>Sorry </p>`)
+  } catch (response) {
+    if (response instanceof TypeError) {
+      return $('ul#list').append(`<li>All the listings for your search</li>`);
     }
   }
 }
@@ -25,27 +24,28 @@ function getElement(response) {
 
 function getElement2(response2) {
   console.log(response2);
-
-  for (let i = 0; i < 10; i++) {
-    if(response2.data[i].practices[0].within_search_area === false){
-      continue;
+  try {
+    for (let i = 0; i < 10; i++) {
+      if (response2.data[i].practices[0].within_search_area === false) {
+        continue;
+      }
+      $('ul#list').append(`<li> ${response2.data[i].profile.first_name} ${response2.data[i].profile.last_name}<p>Address: ${response2.data[i].practices[0].visit_address.street}, ${response2.data[i].practices[0].visit_address.city}, ${response2.data[i].practices[0].visit_address.state_long} ${response2.data[i].practices[0].visit_address.zip} <br>Phone Number: ${response2.data[i].practices[0].phones[0].number} <br>Accepting New Patients: ${response2.data[i].practices[0].accepts_new_patients} </p></li>`);
     }
-    $('ul#list').append(`<li ${response2.data[i].profile.first_name} ${response2.data[i].profile.last_name}<p>Address: ${response2.data[i].practices[0].visit_address.street}, ${response2.data[i].practices[0].visit_address.city}, ${response2.data[i].practices[0].visit_address.state_long} ${response2.data[i].practices[0].visit_address.zip} <br>Phone Number: ${response2.data[i].practices[0].phones[0].number} <br>Accepting New Patients: ${response2.data[i].practices[0].accepts_new_patients} </p></li>`)
+  } catch (response2) {
+    if (response2 instanceof TypeError) {
+      return $('ul#list').append(`<li>All the listings for your search</li>`);
+    }
   }
 }
 
 
-
 $(document).ready(function() {
   $('form#doctorForm').submit(function(event) {
-    // attachListeners();
     event.preventDefault();
     $('ul').html("");
     let doctorNameInput = $('input#doctorNameInput').val();
     let medicalIssueInput = $('input#medicalIssueInput').val();
-    $('input#doctorNameInput, input#medicalIssueInput').val("")
-    console.log("Medical Issue: ", medicalIssueInput);
-    console.log("Doctor Name: ", doctorNameInput);
+    $('input#doctorNameInput, input#medicalIssueInput').val("");
     if (medicalIssueInput === "") {
       (async () => {
         let doctorNameSearch = new DoctorNameSearch();
